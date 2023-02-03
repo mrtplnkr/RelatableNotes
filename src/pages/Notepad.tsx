3f820cdc0527697defc2fc266a28cc7c28fb0f52
martin <m.cepauskas@varutis.lt>
2023-02-03 10:17:08 +0200

    clean up

--- src/pages/Notepad.tsx
+++ src/pages/Notepad.tsx
@@ -4,7 +4,6 @@ import { INote } from '../data/NotepadReducer';
 import { Link } from 'react-router-dom';
 import { useNotepadContext } from '../data/NotepadContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { InitialInputWithTypes } from '../components/organisms/InitialInputWithTypes';
 import { SearchInputWithTypes } from '../components/organisms/SearchInputWithTypes';
 import { faUndo } from '@fortawesome/free-solid-svg-icons';
 import scrollToElement from 'scroll-to-element';
@@ -25,11 +24,8 @@ export const compareLatest = (a: INote, b: INote) => {
 export function Notepad (props: INotepadProps) {
   
   const { notes, found, highlighted, filter, dispatchNotes } = useNotepadContext();
-  // const [selectedType, setSelectedType] = useState<ENoteType>();
 
   const [showOptions, setShowOptions] = useState<number>(0);
-
-  const [searchOrNot, setSearchOrNot] = useState<boolean>(filter.text.length > 0);
 
   const [showAllFound, setShowAllFound] = useState<boolean>(false);
 
@@ -59,43 +55,37 @@ export function Notepad (props: INotepadProps) {
     <div>
       <h3>Your Collections</h3>
       <div style={{justifyContent: 'center'}}>
-          <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-            <div style={{flex: 1}}>
-              {!searchOrNot ? 
-                <InitialInputWithTypes addNote={addNote} setSearchOrNot={setSearchOrNot} /> 
-                :
-                <>
-                  <div style={{display: 'flex', justifyContent: 'center'}}>
-                    <SearchInputWithTypes {...{handleSearch, addNote, searchTerm: filter.text, highlighted: highlighted.length>0}} />
-                    <span style={{alignSelf: 'end', paddingLeft: '1rem'}}>
-                      (<u onClick={(e) => setShowAllFound(true)}>{found ? found.length : 0}</u>)
-                    </span>
-                  </div>
-                  {showAllFound && <ul>
-                    {found && notes.filter(x => found.includes(x.id)).map(x => {
-                      return (
-                        <li style={{ padding: '5px' }} key={x.id}>
-                            <button onClick={() => navigateTo(x.id)}>{x.text}</button>
-                        </li>
-                      );
-                    })}
-                  </ul>}
-                </>
-              }
+        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+          <div style={{flex: 1}}>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              <SearchInputWithTypes {...{handleSearch, addNote, searchTerm: filter.text, highlighted: highlighted.length>0}} />
+              <span style={{alignSelf: 'end', paddingLeft: '1rem'}}>
+                (<u onClick={(e) => setShowAllFound(true)}>{found ? found.length : 0}</u>)
+              </span>
             </div>
+            {showAllFound && <ul>
+              {found && notes.filter(x => found.includes(x.id)).map(x => {
+                return (
+                  <li style={{ padding: '5px' }} key={x.id}>
+                      <button onClick={() => navigateTo(x.id)}>{x.text}</button>
+                  </li>
+                );
+              })}
+            </ul>}
           </div>
-          <hr />
-          {notes.some(x => x.cut) && <span style={{ position: 'absolute', right: 0 }}>
-              {notes.find(x => x.cut)!.text}
-              <FontAwesomeIcon onClick={() => dispatchNotes({ type: 'cancelCut', payload: notes[0] })} title='undo' cursor={'pointer'} icon={faUndo} style={{ padding: '0 0.5em' }} />
-          </span>}
-          {notes.filter((x: INote) => x.parentId === null).sort(compareLatest).map((x, index) => {
-              return (
-                  <div style={{ padding: '5px' }} key={x.id}>
-                      <ReusableObject {...{ showOptions, setShowOptions, dispatch: dispatchNotes }} mainNote={x} size={18}></ReusableObject>
-                  </div>
-              );
-          })}
+        </div>
+        <hr />
+        {notes.some(x => x.cut) && <span style={{ position: 'absolute', right: 0 }}>
+            {notes.find(x => x.cut)!.text}
+            <FontAwesomeIcon onClick={() => dispatchNotes({ type: 'cancelCut', payload: notes[0] })} title='undo' cursor={'pointer'} icon={faUndo} style={{ padding: '0 0.5em' }} />
+        </span>}
+        {notes.filter((x: INote) => x.parentId === null).sort(compareLatest).map((x, index) => {
+            return (
+                <div style={{ padding: '5px' }} key={x.id}>
+                    <ReusableObject {...{ showOptions, setShowOptions, dispatch: dispatchNotes }} mainNote={x} size={18}></ReusableObject>
+                </div>
+            );
+        })}
       </div>
       <p>
         <Link to="/dataPreview">get visuals</Link>
